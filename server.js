b1e3a4f562cc80326f6b1a5f16ea2f74cc508c8b
Jake O'Toole <otoolej33@gmail.com>
2021-03-18 21:00:09 -0500

    Removed the build.

--- server.js
+++ server.js
@@ -1,4 +1,4 @@
-const path = require('path');
+const path = require("path");
 const express = require("express");
 const session = require("express-session");
 const mongoose = require("mongoose");
@@ -8,7 +8,9 @@ const passport = require("./config/passport");
 const routes = require("./routes");
 
 // create sessions
-app.use(session({ secret: "keyboard cat", resave: true, saveUninitialized: true }));
+app.use(
+  session({ secret: "keyboard cat", resave: true, saveUninitialized: true })
+);
 app.use(passport.initialize());
 app.use(passport.session());
 
@@ -24,10 +26,14 @@ if (process.env.NODE_ENV === "production") {
 // Add routes
 app.use(routes);
 
+router.use("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "../client/build/index.html"));
+});
+
 // Connect to the Mongo DB
 mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/warp_point");
 
 // Start the API server
-app.listen(PORT, function() {
+app.listen(PORT, function () {
   console.log(`🌎  ==> API Server now listening on PORT ${PORT}!`);
 });
